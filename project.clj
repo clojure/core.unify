bf6fa09a264d12624bd38bdc67c6265c76992782
fogus <mefogus@gmail.com>
2012-08-11 15:36:14 -0400

    playing with ideas for the next API version

--- project.clj
+++ project.clj
@@ -2,7 +2,6 @@
   :description "Clojure unification library."
   :dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]
   :dev-dependencies [[jline "0.9.94"]
-                     [swank-clojure "1.4.2"]
                      [lein-marginalia "0.7.1"]
                      [lein-multi "1.1.0"]]
   :multi-deps {"1.2"   [[org.clojure/clojure "1.2.0"]]
@@ -10,6 +9,7 @@
                "1.3"   [[org.clojure/clojure "1.3.0"]]
                "1.4"   [[org.clojure/clojure "1.4.0"]]
                "1.5"   [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}
-  :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
-  :source-path "src/main/clojure"
-  :test-path   "src/test/clojure")
+  :plugins [[lein-swank "1.4.4"]]
+  :repositories {"sonatype-oss-public" {:url "https://oss.sonatype.org/content/groups/public/"}}
+  :source-paths ["src/main/clojure"]
+  :test-paths   ["src/test/clojure"])
--- src/main/clojure/clojure/core/unify.clj
+++ src/main/clojure/clojure/core/unify.clj
@@ -257,5 +257,62 @@
   (def gen (make-genr "http://foo.com/foo/"))
   
   (gen T '{?a 1, ?b 2})
-  ;=> "http://foo.com/foo/1/2"
+  ;;=> "http://foo.com/foo/1/2"
+
+  (unify {:first '?first
+          :last  '?last
+          :genre :giallo}
+
+         {:first "Dario"
+          :last  "Argento"
+          :genre :giallo})
+
+  (unifier ['?first '?last]
+           '{?first "Dario", ?last ?first})
+
+
+  (subst '[1 2 ?x ?y]
+         '{?x [3 4 ?x 6]})
+
+  (unify '{:first ?nickname
+           :last  ?last
+           :genre :giallo}
+
+         '{:last  "Argento"
+           :genre :giallo})
+
+
+    (subst '[1 2 ?x ?y]
+           '{?x [3 4 ?y 6]})
+
+
+    (unifier '[(?a * ?x | 2) + (?b * ?x) + ?c]
+             '[?z + (4 * 5) + 3])
+
+    (unify '[(?a * ?x | 2) + (?b * ?x) + ?c]
+           '[?z + (4 * 5) + 3])
+    
+
+    (unify '[(?a * ?x | 2) + (?b * ?x) + ?c]
+           '[(?a * 5 | 2) + (4 * 5) + 3])
+
+    ;;=> {?c 3, ?b 4, ?x 5}
+
+    (unify '[(?a * 5 | 2) + (4 * 5) + 3]
+           '[?z + (4 * 5) + 3])
+
+    ;;=> {?z (?a * 5 | 2)}
+
+    (= (subst '[?z + (4 * 5) + 3]
+              '{?c 3, ?b 4, ?x 5
+                ?z (?a * 5 | 2)})
+       
+       (subst '[(?a * ?x | 2) + (?b * ?x) + ?c]
+              '{?c 3, ?b 4, ?x 5
+                ?z (?a * 5 | 2)}))
+
+    ;;=> true
+
+    (subst '[(?a * ?x | 2) + (?b * ?x) + ?c]
+           '{?c 3, ?b 4, ?x 5})
 )
