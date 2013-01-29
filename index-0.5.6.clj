5dee98f1d24152664ac4f1f6f3f5a1e439446578
Tom Faulhaber <git_net@infolace.com>
2013-01-29 15:55:41 -0800

    Autodoc commit for master/81eecdb2

--- index-0.5.6.clj
+++ index-0.5.6.clj
@@ -1,163 +1,191 @@
 {:namespaces
  ({:source-url
-   "https://github.com/clojure/core.unify/blob/bf6fa09a264d12624bd38bdc67c6265c76992782/src/main/clojure/clojure/core/unify.clj",
+   "https://github.com/clojure/core.unify/blob/81eecdb2ff5d6654627fb9074922665209212d97/src/main/clojure/clojure/core/unify.clj",
    :wiki-url
    "http://clojure.github.com/core.unify/clojure.core.unify-api.html",
    :name "clojure.core.unify",
    :author "Michael Fogus",
    :doc "A unification library for Clojure."}),
  :vars
- ({:arglists ([variable-fn]),
+ ({:arglists ([form] [lv-fn form]),
+   :name "extract-lvars",
+   :namespace "clojure.core.unify",
+   :source-url
+   "https://github.com/clojure/core.unify/blob/81eecdb2ff5d6654627fb9074922665209212d97/src/main/clojure/clojure/core/unify.clj#L22",
+   :raw-source-url
+   "https://github.com/clojure/core.unify/raw/81eecdb2ff5d6654627fb9074922665209212d97/src/main/clojure/clojure/core/unify.clj",
+   :wiki-url
+   "http://clojure.github.com/core.unify//clojure.core.unify-api.html#clojure.core.unify/extract-lvars",
+   :doc
+   "Takes a datastructure and returns a distinct set of the logical\nvariables found within.",
+   :var-type "function",
+   :line 22,
+   :file "src/main/clojure/clojure/core/unify.clj"}
+  {:arglists ([binds] [variable? binds]),
+   :name "flatten-bindings",
+   :namespace "clojure.core.unify",
+   :source-url
+   "https://github.com/clojure/core.unify/blob/81eecdb2ff5d6654627fb9074922665209212d97/src/main/clojure/clojure/core/unify.clj#L131",
+   :raw-source-url
+   "https://github.com/clojure/core.unify/raw/81eecdb2ff5d6654627fb9074922665209212d97/src/main/clojure/clojure/core/unify.clj",
+   :wiki-url
+   "http://clojure.github.com/core.unify//clojure.core.unify-api.html#clojure.core.unify/flatten-bindings",
+   :doc
+   "Flattens recursive bindings in the given map to the same ground (if possible).",
+   :var-type "function",
+   :line 131,
+   :file "src/main/clojure/clojure/core/unify.clj"}
+  {:arglists ([variable-fn]),
    :name "make-occurs-subst-fn",
    :namespace "clojure.core.unify",
    :source-url
-   "https://github.com/clojure/core.unify/blob/bf6fa09a264d12624bd38bdc67c6265c76992782/src/main/clojure/clojure/core/unify.clj#L161",
+   "https://github.com/clojure/core.unify/blob/81eecdb2ff5d6654627fb9074922665209212d97/src/main/clojure/clojure/core/unify.clj#L178",
    :raw-source-url
-   "https://github.com/clojure/core.unify/raw/bf6fa09a264d12624bd38bdc67c6265c76992782/src/main/clojure/clojure/core/unify.clj",
+   "https://github.com/clojure/core.unify/raw/81eecdb2ff5d6654627fb9074922665209212d97/src/main/clojure/clojure/core/unify.clj",
    :wiki-url
    "http://clojure.github.com/core.unify//clojure.core.unify-api.html#clojure.core.unify/make-occurs-subst-fn",
    :doc
    "Given a function to recognize unification variables, returns a function that\nwill attempt to substitute unification bindings between two expressions.\nThis function uses an 'occurs check' methodology for detecting cycles.",
    :var-type "function",
-   :line 161,
+   :line 178,
    :file "src/main/clojure/clojure/core/unify.clj"}
   {:arglists ([variable-fn]),
    :name "make-occurs-unifier-fn",
    :namespace "clojure.core.unify",
    :source-url
-   "https://github.com/clojure/core.unify/blob/bf6fa09a264d12624bd38bdc67c6265c76992782/src/main/clojure/clojure/core/unify.clj#L168",
+   "https://github.com/clojure/core.unify/blob/81eecdb2ff5d6654627fb9074922665209212d97/src/main/clojure/clojure/core/unify.clj#L185",
    :raw-source-url
-   "https://github.com/clojure/core.unify/raw/bf6fa09a264d12624bd38bdc67c6265c76992782/src/main/clojure/clojure/core/unify.clj",
+   "https://github.com/clojure/core.unify/raw/81eecdb2ff5d6654627fb9074922665209212d97/src/main/clojure/clojure/core/unify.clj",
    :wiki-url
    "http://clojure.github.com/core.unify//clojure.core.unify-api.html#clojure.core.unify/make-occurs-unifier-fn",
    :doc
    "Given a function to recognize unification variables, returns a function to\nperform the unification of two expressions. This function uses an 'occurs check'\nmethodology for detecting cycles.",
    :var-type "function",
-   :line 168,
+   :line 185,
    :file "src/main/clojure/clojure/core/unify.clj"}
   {:arglists ([variable-fn]),
    :name "make-occurs-unify-fn",
    :namespace "clojure.core.unify",
    :source-url
-   "https://github.com/clojure/core.unify/blob/bf6fa09a264d12624bd38bdc67c6265c76992782/src/main/clojure/clojure/core/unify.clj#L154",
+   "https://github.com/clojure/core.unify/blob/81eecdb2ff5d6654627fb9074922665209212d97/src/main/clojure/clojure/core/unify.clj#L169",
    :raw-source-url
-   "https://github.com/clojure/core.unify/raw/bf6fa09a264d12624bd38bdc67c6265c76992782/src/main/clojure/clojure/core/unify.clj",
+   "https://github.com/clojure/core.unify/raw/81eecdb2ff5d6654627fb9074922665209212d97/src/main/clojure/clojure/core/unify.clj",
    :wiki-url
    "http://clojure.github.com/core.unify//clojure.core.unify-api.html#clojure.core.unify/make-occurs-unify-fn",
    :doc
    "Given a function to recognize unification variables, returns a function to\nreturn a bindings map for two expressions.  This function uses an 'occurs check'\nmethodology for detecting cycles.",
    :var-type "function",
-   :line 154,
+   :line 169,
    :file "src/main/clojure/clojure/core/unify.clj"}
   {:arglists ([variable-fn]),
    :name "make-subst-fn",
    :namespace "clojure.core.unify",
    :source-url
-   "https://github.com/clojure/core.unify/blob/bf6fa09a264d12624bd38bdc67c6265c76992782/src/main/clojure/clojure/core/unify.clj#L198",
+   "https://github.com/clojure/core.unify/blob/81eecdb2ff5d6654627fb9074922665209212d97/src/main/clojure/clojure/core/unify.clj#L217",
    :raw-source-url
-   "https://github.com/clojure/core.unify/raw/bf6fa09a264d12624bd38bdc67c6265c76992782/src/main/clojure/clojure/core/unify.clj",
+   "https://github.com/clojure/core.unify/raw/81eecdb2ff5d6654627fb9074922665209212d97/src/main/clojure/clojure/core/unify.clj",
    :wiki-url
    "http://clojure.github.com/core.unify//clojure.core.unify-api.html#clojure.core.unify/make-subst-fn",
    :doc
    "Given a function to recognize unification variables, returns a function that\nwill attempt to substitute unification bindings between two expressions.",
    :var-type "function",
-   :line 198,
+   :line 217,
    :file "src/main/clojure/clojure/core/unify.clj"}
   {:arglists ([variable-fn]),
    :name "make-unifier-fn",
    :namespace "clojure.core.unify",
    :source-url
-   "https://github.com/clojure/core.unify/blob/bf6fa09a264d12624bd38bdc67c6265c76992782/src/main/clojure/clojure/core/unify.clj#L204",
+   "https://github.com/clojure/core.unify/blob/81eecdb2ff5d6654627fb9074922665209212d97/src/main/clojure/clojure/core/unify.clj#L223",
    :raw-source-url
-   "https://github.com/clojure/core.unify/raw/bf6fa09a264d12624bd38bdc67c6265c76992782/src/main/clojure/clojure/core/unify.clj",
+   "https://github.com/clojure/core.unify/raw/81eecdb2ff5d6654627fb9074922665209212d97/src/main/clojure/clojure/core/unify.clj",
    :wiki-url
    "http://clojure.github.com/core.unify//clojure.core.unify-api.html#clojure.core.unify/make-unifier-fn",
    :doc
    "Given a function to recognize unification variables, returns a function to\nperform the unification of two expressions.",
    :var-type "function",
-   :line 204,
+   :line 223,
    :file "src/main/clojure/clojure/core/unify.clj"}
   {:arglists ([variable-fn]),
    :name "make-unify-fn",
    :namespace "clojure.core.unify",
    :source-url
-   "https://github.com/clojure/core.unify/blob/bf6fa09a264d12624bd38bdc67c6265c76992782/src/main/clojure/clojure/core/unify.clj#L192",
+   "https://github.com/clojure/core.unify/blob/81eecdb2ff5d6654627fb9074922665209212d97/src/main/clojure/clojure/core/unify.clj#L209",
    :raw-source-url
-   "https://github.com/clojure/core.unify/raw/bf6fa09a264d12624bd38bdc67c6265c76992782/src/main/clojure/clojure/core/unify.clj",
+   "https://github.com/clojure/core.unify/raw/81eecdb2ff5d6654627fb9074922665209212d97/src/main/clojure/clojure/core/unify.clj",
    :wiki-url
    "http://clojure.github.com/core.unify//clojure.core.unify-api.html#clojure.core.unify/make-unify-fn",
    :doc
    "Given a function to recognize unification variables, returns a function to\nreturn a bindings map for two expressions.",
    :var-type "function",
-   :line 192,
+   :line 209,
    :file "src/main/clojure/clojure/core/unify.clj"}
   {:arglists ([expression bindings]),
    :name "subst",
    :namespace "clojure.core.unify",
    :source-url
-   "https://github.com/clojure/core.unify/blob/bf6fa09a264d12624bd38bdc67c6265c76992782/src/main/clojure/clojure/core/unify.clj#L181",
+   "https://github.com/clojure/core.unify/blob/81eecdb2ff5d6654627fb9074922665209212d97/src/main/clojure/clojure/core/unify.clj#L198",
    :raw-source-url
-   "https://github.com/clojure/core.unify/raw/bf6fa09a264d12624bd38bdc67c6265c76992782/src/main/clojure/clojure/core/unify.clj",
+   "https://github.com/clojure/core.unify/raw/81eecdb2ff5d6654627fb9074922665209212d97/src/main/clojure/clojure/core/unify.clj",
    :wiki-url
    "http://clojure.github.com/core.unify//clojure.core.unify-api.html#clojure.core.unify/subst",
    :doc
    "Attempts to substitute the bindings in the appropriate locations in the given expression.",
    :var-type "function",
-   :line 181,
+   :line 198,
    :file "src/main/clojure/clojure/core/unify.clj"}
   {:arglists ([expression1 expression2]),
    :name "unifier",
    :namespace "clojure.core.unify",
    :source-url
-   "https://github.com/clojure/core.unify/blob/bf6fa09a264d12624bd38bdc67c6265c76992782/src/main/clojure/clojure/core/unify.clj#L185",
+   "https://github.com/clojure/core.unify/blob/81eecdb2ff5d6654627fb9074922665209212d97/src/main/clojure/clojure/core/unify.clj#L202",
    :raw-source-url
-   "https://github.com/clojure/core.unify/raw/bf6fa09a264d12624bd38bdc67c6265c76992782/src/main/clojure/clojure/core/unify.clj",
+   "https://github.com/clojure/core.unify/raw/81eecdb2ff5d6654627fb9074922665209212d97/src/main/clojure/clojure/core/unify.clj",
    :wiki-url
    "http://clojure.github.com/core.unify//clojure.core.unify-api.html#clojure.core.unify/unifier",
    :doc
    "Attempts the entire unification process from garnering the bindings to substituting\nthe appropriate bindings.  Note: This function is implemented with an occurs-check.",
    :var-type "function",
-   :line 185,
+   :line 202,
    :file "src/main/clojure/clojure/core/unify.clj"}
   {:arglists ([expression1 expression2]),
    :name "unifier-",
    :namespace "clojure.core.unify",
    :source-url
-   "https://github.com/clojure/core.unify/blob/bf6fa09a264d12624bd38bdc67c6265c76992782/src/main/clojure/clojure/core/unify.clj#L221",
+   "https://github.com/clojure/core.unify/blob/81eecdb2ff5d6654627fb9074922665209212d97/src/main/clojure/clojure/core/unify.clj#L240",
    :raw-source-url
-   "https://github.com/clojure/core.unify/raw/bf6fa09a264d12624bd38bdc67c6265c76992782/src/main/clojure/clojure/core/unify.clj",
+   "https://github.com/clojure/core.unify/raw/81eecdb2ff5d6654627fb9074922665209212d97/src/main/clojure/clojure/core/unify.clj",
    :wiki-url
    "http://clojure.github.com/core.unify//clojure.core.unify-api.html#clojure.core.unify/unifier-",
    :doc
    "Attempts the entire unification process from garnering the bindings to substituting\nthe appropriate bindings.  Note: This function is implemented **without** an occurs-check.",
    :var-type "function",
-   :line 221,
+   :line 240,
    :file "src/main/clojure/clojure/core/unify.clj"}
   {:arglists ([expression1 expression2]),
    :name "unify",
    :namespace "clojure.core.unify",
    :source-url
-   "https://github.com/clojure/core.unify/blob/bf6fa09a264d12624bd38bdc67c6265c76992782/src/main/clojure/clojure/core/unify.clj#L176",
+   "https://github.com/clojure/core.unify/blob/81eecdb2ff5d6654627fb9074922665209212d97/src/main/clojure/clojure/core/unify.clj#L193",
    :raw-source-url
-   "https://github.com/clojure/core.unify/raw/bf6fa09a264d12624bd38bdc67c6265c76992782/src/main/clojure/clojure/core/unify.clj",
+   "https://github.com/clojure/core.unify/raw/81eecdb2ff5d6654627fb9074922665209212d97/src/main/clojure/clojure/core/unify.clj",
    :wiki-url
    "http://clojure.github.com/core.unify//clojure.core.unify-api.html#clojure.core.unify/unify",
    :doc
    "Attempt to unify x and y with the given bindings (if any). Potentially returns a map of the \nunifiers (bindings) found.  Will throw an `IllegalStateException` if the expressions\ncontain a cycle relationship.  Will also throw an `IllegalArgumentException` if the\nsub-expressions clash.  Note: This function is implemented with an occurs-check.",
    :var-type "function",
-   :line 176,
+   :line 193,
    :file "src/main/clojure/clojure/core/unify.clj"}
   {:arglists ([expression1 expression2]),
    :name "unify-",
    :namespace "clojure.core.unify",
    :source-url
-   "https://github.com/clojure/core.unify/blob/bf6fa09a264d12624bd38bdc67c6265c76992782/src/main/clojure/clojure/core/unify.clj#L215",
+   "https://github.com/clojure/core.unify/blob/81eecdb2ff5d6654627fb9074922665209212d97/src/main/clojure/clojure/core/unify.clj#L234",
    :raw-source-url
-   "https://github.com/clojure/core.unify/raw/bf6fa09a264d12624bd38bdc67c6265c76992782/src/main/clojure/clojure/core/unify.clj",
+   "https://github.com/clojure/core.unify/raw/81eecdb2ff5d6654627fb9074922665209212d97/src/main/clojure/clojure/core/unify.clj",
    :wiki-url
    "http://clojure.github.com/core.unify//clojure.core.unify-api.html#clojure.core.unify/unify-",
    :doc
    "Attempt to unify x and y with the given bindings (if any). Potentially returns a map of the \nunifiers (bindings) found.  Will throw an `IllegalStateException` if the expressions\ncontain a cycle relationship.  Will also throw an `IllegalArgumentException` if the\nsub-expressions clash.  Note: This function is implemented **without** an occurs-check.",
    :var-type "function",
-   :line 215,
+   :line 234,
    :file "src/main/clojure/clojure/core/unify.clj"})}
