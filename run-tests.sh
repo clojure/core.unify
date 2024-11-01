01fb301422595a270dd6864bbf3c20f96440397f
Sean Corfield <sean@corfield.org>
2024-11-01 15:22:31 -0700

    update dependencies; add simpler cljs testing
    Signed-off-by: Sean Corfield <sean@corfield.org>

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-versions="1.7 1.8 1.9 1.10 1.11 1.12"
+time clojure -M:test:runner-1.7:1.7
+versions="1.8 1.9 1.10 1.11 1.12"
 for v in $versions
 do
-  time clj -M:test:runner:$v
+  time clojure -M:test:runner:$v
 done
+time clojure -M:test:cljs-runner
